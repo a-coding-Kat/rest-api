f001606640538ce5c2fc87b90918bb0dd47b00a4
Kat <72339086+a-coding-Kat@users.noreply.github.com>
2022-10-27 00:24:18 +0200

    Reduce data model

--- wdb_rest/server.py
+++ wdb_rest/server.py
@@ -13,22 +13,11 @@ class TrackModel(db.Model):
     id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
     track = db.Column('track', db.String, nullable=False)
     artist = db.Column('artist', db.String, nullable=False)
-    uri = db.Column('uri', db.String, nullable=False)
     danceability = db.Column('danceability', db.Float, nullable=False)
-    energy = db.Column('energy', db.Float, nullable=False)
-    key = db.Column('key', db.Integer, nullable=False)
     loudness = db.Column('loudness', db.Float, nullable=False)
-    mode = db.Column('mode', db.Integer, nullable=False)
-    speechiness = db.Column('speechiness', db.Float, nullable=False)
-    acousticness = db.Column('acousticness', db.Float, nullable=False)
     instrumentalness = db.Column('instrumentalness', db.Float, nullable=False)
-    liveness = db.Column('liveness', db.Float, nullable=False)
-    valence = db.Column('valence', db.Float, nullable=False)
     tempo = db.Column('tempo', db.Float, nullable=False)
     duration_ms = db.Column('duration_ms', db.Float, nullable=False)
-    time_signature = db.Column('time_signature', db.Integer, nullable=False)
-    chorus_hit = db.Column('chorus_hit', db.Float, nullable=False)
-    sections = db.Column('sections', db.Integer, nullable=False)
     popularity = db.Column('popularity', db.Integer, nullable=False)
     decade = db.Column('decade', db.String, nullable=False)
 
@@ -38,22 +27,11 @@ track_fields = {
     'id': fields.Integer,
     'track': fields.String,
     'artist': fields.String,
-    'uri': fields.String,
     'danceability': fields.Float,
-    'energy': fields.Float,
-    'key': fields.Integer,
     'loudness': fields.Float,
-    'mode': fields.Integer,
-    'speechiness': fields.Float,
-    'acousticness': fields.Float,
     'instrumentalness': fields.Float,
-    'liveness': fields.Float,
-    'valence': fields.Float,
     'tempo': fields.Float,
     'duration_ms': fields.Float,
-    'time_signature': fields.Integer,
-    'chorus_hit': fields.Float,
-    'sections': fields.Integer,
     'popularity': fields.Integer,
     'decade': fields.String
 }
