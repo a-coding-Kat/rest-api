8129c106c64aab583348b815f1ff5bfbfd3d2266
Kat <72339086+a-coding-Kat@users.noreply.github.com>
2022-10-27 00:46:36 +0200

    Add create method (and parser)

--- wdb_rest/server.py
+++ wdb_rest/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from flask_restful import Api, Resource, marshal_with, fields
+from flask_restful import Api, Resource, marshal_with, fields, reqparse
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -36,10 +36,28 @@ track_fields = {
     'decade': fields.String
 }
 
+# Parse though the request to check if it meets the defined guidelines.
+track_put_args = reqparse.RequestParser()
+track_put_args.add_argument('track', type=str, help='Name of the track is required.', required=True)
+track_put_args.add_argument('artist', type=str, help='Name of the artist is required.', required=True)
+track_put_args.add_argument('danceability', type=float, help='Danceability is required.', required=True)
+track_put_args.add_argument('loudness', type=float, help='Loudness is required.', required=True)
+track_put_args.add_argument('instrumentalness', type=float, help='Instrumentalness is required.', required=True)
+track_put_args.add_argument('tempo', type=float, help='Tempo is required.', required=True)
+track_put_args.add_argument('duration_ms', type=float, help='Duration is required.', required=True)
+track_put_args.add_argument('popularity', type=int, help='Popularity is required.', required=True)
+track_put_args.add_argument('decade', type=str, help='Decade is required.', required=True)
+
+
 class Track(Resource):
     @marshal_with(track_fields)
     def put(self, track_id):
-        return {}
+        args = track_put_args.parse_args()
+        track = TrackModel(**args)
+        # Adding entries permanently to the database.
+        db.session.add(track)
+        db.session.flush() # db.session.commit closes session, does not allow to return tracks
+        return track, 201
 
     @marshal_with(track_fields) # serializes objects of the method
     def get(self, track_id):
@@ -70,6 +88,7 @@ api.add_resource(Track, '/track/<int:track_id>')
 # Exception handling
 @app.errorhandler(Exception)
 def handle_exception(e):
+    print(e)
     return {'msg': str(e)}, 500
 
 
