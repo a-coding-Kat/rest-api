aa2e2397e1557ae5170b64490ec80cf912d18580
Kat <72339086+a-coding-Kat@users.noreply.github.com>
2022-10-27 01:23:07 +0200

    Add client

--- wdb_rest/client.py
+++ wdb_rest/client.py
@@ -1,4 +1,24 @@
 import requests
 
-r = requests.get('http://127.0.0.1:5000/tracks')
-print(r.json())
+default_url = 'http://127.0.0.1:5000/'
+
+
+class TrackClient:
+    def __init__(self, url=default_url):
+        self.url = url
+
+    def create_track(self, track):
+        r = requests.put(self.url + 'track/0', track)
+        return r.json(), r.status_code
+
+    def get_track(self, track_id):
+        r = requests.get(self.url + 'track/' + str(track_id))
+        return r.json(), r.status_code
+
+    def update_track(self, track):
+        r = requests.patch(self.url + 'track/' + str(track['track_id']), track)
+        return r.json(), r.status_code
+
+    def delete_track(self, track_id):
+        r = requests.delete(self.url + 'track/' + str(track_id))
+        return r.json(), r.status_code
