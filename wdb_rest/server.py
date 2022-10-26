b1ad267e9f15c617ff2692c502befb72ee0250ae
Kat <72339086+a-coding-Kat@users.noreply.github.com>
2022-10-26 21:17:24 +0200

    Create track model

--- wdb_rest/server.py
+++ wdb_rest/server.py
@@ -1,19 +1,30 @@
-import pandas as pd
-import data
-from flask import Flask, jsonify
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask("wdb_api")
+app = Flask(__name__)
+api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-
-@app.route("/tracks")
-def get_tracks():
-    result = data.get_tracks()
-    return jsonify(result)
-
+class TrackModel(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    popularity = db.Column(db.Integer, nullable=False)
+    danceability = db.Column(db.Float, nullable=False)
+    energy = db.Column(db.Float, nullable=False)
+    key = db.Column(db.Integer, nullable=False)
+    loudness = db.Column(db.Float, nullable=False)
+    mode = db.Column(db.Integer, nullable=False)
+    speechiness = db.Column(db.Float, nullable=False)
+    acousticness = db.Column(db.Float, nullable=False)
+    instrumentalness = db.Column(db.Float, nullable=False)
+    liveness = db.Column(db.Float, nullable=False)
+    valence = db.Column(db.Float, nullable=False)
+    tempo = db.Column(db.Float, nullable=False)
+    duration_ms = db.Column(db.Float, nullable=False)
+    time_signature = db.Column(db.Integer, nullable=False)
 
 app.run(debug=True)
