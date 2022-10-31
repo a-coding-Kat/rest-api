131d956a985473461883ce22b200527c59b45101
Kat <72339086+a-coding-Kat@users.noreply.github.com>
2022-10-31 22:13:10 +0100

    Temporary remove multiple databases option

--- import_data.py
+++ import_data.py
@@ -12,21 +12,20 @@ df = df.rename(columns={'index': 'id'})
 
 # Connect to database.
 conn = sqlite3.connect('database.db')
-conn_test = sqlite3.connect('database-test.db')
 
 # Create table needed for import from Pandas dataframe.
 create_table_sql = '''
 CREATE TABLE "track_model" (
   "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "track" TEXT,
-  "artist" TEXT,
-  "danceability" REAL,
-  "key" INTEGER,
-  "instrumentalness" REAL,
-  "tempo" REAL,
-  "duration_ms" INTEGER,
-  "popularity" INTEGER,
-  "decade" TEXT
+  "track" TEXT NOT NULL,
+  "artist" TEXT NOT NULL,
+  "danceability" REAL NOT NULL,
+  "key" INTEGER NOT NULL,
+  "instrumentalness" REAL NOT NULL,
+  "tempo" REAL NOT NULL,
+  "duration_ms" INTEGER NOT NULL,
+  "popularity" INTEGER NOT NULL,
+  "decade" TEXT NOT NULL
 )
 '''
 
@@ -37,16 +36,7 @@ c.execute("DROP TABLE IF EXISTS track_model")
 # Execute create table query.
 c.execute(create_table_sql)
 
-c_test = conn_test.cursor()
-c_test.execute("DROP TABLE IF EXISTS track_model")
-c_test.execute(create_table_sql)
-
 # Store pandas df to database. Select only columns of interest.
 df = pd.read_csv('data/spotify_dataset.csv')[['track', 'artist', 'danceability', 'key', 'instrumentalness',
                                               'tempo', 'duration_ms', 'popularity', 'decade']]
 df.to_sql('track_model', conn, if_exists='append', index=False)
-df.to_sql('track_model', conn_test, if_exists='append', index=False)
-
-
-
-
